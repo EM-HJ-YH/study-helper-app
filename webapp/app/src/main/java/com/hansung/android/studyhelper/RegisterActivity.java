cf87f9ebec2d3993ab3a3b26d6e1ee627891eabb
EunmiLee <eunmi984@gmail.com>
2018-11-13 10:33:23 +0900

    Initial commit

---
+++ webapp/app/src/main/java/com/hansung/android/studyhelper/RegisterActivity.java
@@ -0,0 +1,8 @@
+package com.hansung.android.studyhelper;
+
+/**
+ * Created by leeem on 2018-11-12.
+ */
+
+public class RegisterActivity {
+}
---
+++ webapp/app/src/main/java/com/hansung/android/studyhelper/SignInActivity.java
@@ -0,0 +1,8 @@
+package com.hansung.android.studyhelper;
+
+/**
+ * Created by leeem on 2018-11-13.
+ */
+
+public class SignInActivity {
+}
